063b9b26e32b815d24ab9193ecc637d620a82f4f
JelleKUL <jelle.vermadere@kuleuven.be>
2026-03-03 09:13:14 +0100

    completion update

--- drm/__init__.py
+++ drm/__init__.py
@@ -183,18 +183,16 @@ def txt_pcd_to_ply(txt_path, ply_path):
         f.write("property float x\n")
         f.write("property float y\n")
         f.write("property float z\n")
-        f.write("property float nx\n")
-        f.write("property float ny\n")
-        f.write("property float nz\n")
+        if len(rows[0]) == 9:
+            f.write("property float nx\n")
+            f.write("property float ny\n")
+            f.write("property float nz\n")
         f.write("property uchar red\n")
         f.write("property uchar green\n")
         f.write("property uchar blue\n")
         f.write("end_header\n")
 
         for r in rows:
-            if len(r) != 9:
-                raise ValueError(f"Expected 9 values per row, got {len(r)}")
-
             f.write(" ".join(r) + "\n")
 
 
